98f5d0187cf929ad95ae48a13931f4af68326841
rthomas86 <rebkthomas72@gmail.com>
2024-05-09 17:01:53 -0700

    added SESSION_SECRET to the .env file

--- test/server.js
+++ test/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 const sess = {
-  secret: "MVC Tech Blog secret",
+  secret: process.env.SESSION_SECRET,
   cookie: {
     maxAge: 36000,
     httpOnly: true,
