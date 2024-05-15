100206110b1744d898b4f2a51588dd675894973c
rthomas86 <rebkthomas72@gmail.com>
2024-05-14 18:00:17 -0700

    adding user session, signup, signin, and logout

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,6 +4,43 @@ const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
   try {
+    // Get all post and JOIN with user data
+    const userData = await User.findAll({});
+
+    // Serialize data so the template can read it
+    const users = userData.map((user) => user.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render("homepage", { users, logged_in: req.session.logged_in });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("signup");
+});
+
+/*
+router.get("/", async (req, res) => {
+  try {
+
     // Get all projects and JOIN with user data
     const testData = await TestTable.findAll({
       include: [
@@ -25,5 +62,6 @@ router.get("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+*/
 
 module.exports = router;
