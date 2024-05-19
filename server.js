442107c299856852c7b87ea70ff2c6c180398d38
AntCamper <152764140+AntCamper@users.noreply.github.com>
2024-05-19 15:54:20 -0700

    setup my .env for the database credentials added bcrypt for password hashing I defined the user model and added some hooks to has the passwords defined a test table to go with the user model express is setup to serve static files from public

--- server.js
+++ server.js
@@ -1,44 +1,90 @@
 const express = require('express');
-//const session = require('express-session');
-//const cookieParser = require('cookie-parser');
-const handlebars = require('express-handlebars');
-const sequelize = require('sequelize');
-require('dotenv').config();
+const bcrypt = require('bcrypt');
+const dotenv = require('dotenv');
+const { Sequelize, DataTypes } = require('sequelize');
+const path = require('path');
+
+dotenv.config();
 
 const app = express();
 
-// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser());
-// app.use(session({
-//     secret: process.env.SESSION_SECRET,
-//     resave: false,
-//     saveUninitialized: false
-// }));
 
-// Set up Handlebars as the template engine
-// app.engine('handlebars', handlebars());
-// app.set('view engine', 'handlebars');
-
-// Set up MySQL and Sequelize
-const { Sequelize, DataTypes } = sequelize;
 const sequelizeInstance = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: process.env.DB_DIALECT || 'mysql'
 });
 
-// Define Model
-const Data = sequelizeInstance.define('Data', {
-    // Define your model fields here
+const saltRounds = 10;
+
+const User = sequelizeInstance.define('User', {
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
 });
 
-// Routes
+User.beforeCreate(async (user, options) => {
+    user.password = await bcrypt.hash(user.password, saltRounds);
+});
+
+User.beforeUpdate(async (user, options) => {
+    if (user.changed('password')) {
+        user.password = await bcrypt.hash(user.password, saltRounds);
+    }
+});
+
+const TestTable = sequelizeInstance.define('TestTable', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: User,
+            key: 'id'
+        }
+    }
+});
+
+User.hasMany(TestTable, {
+    foreignKey: 'user_id',
+});
+
+TestTable.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'cascade',
+});
+
+sequelizeInstance.sync().then(() => {
+    console.log('Database & tables created!');
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/signup', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.create({ username, password });
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
 const dataRoutes = require('./routes/dataRoutes');
 app.use('/api/data', dataRoutes);
 
-// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
