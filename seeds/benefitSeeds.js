2a4c72c4acdc911890cdd9906acdca54f1b37fed
Jenina <jeninadelacruz521@yahoo.com>
2024-05-20 03:24:49 -0700

    created benefitseeds, added to button

--- seeds/benefitSeeds.js
+++ seeds/benefitSeeds.js
@@ -15,7 +15,23 @@ const benefitsData = [
   },
   {
     name: 'Lowered Blood Pressure',
-    description: 'By activating the body\'s relaxation response...',
+    description: 'By activating the bodys relaxation response, deep breathing can help lower blood pressure and promote cardiovascular health.',
+  },
+  {
+    name: 'Improved Oxygenation',
+    description: ' Deep breathing allows you to take in more oxygen, which is essential for optimal functioning of the body and brain. This can enhance energy levels and cognitive function.',
+  },
+  {
+    name: 'Enhanced Relaxation',
+    description: 'Deep breathing can help you relax and unwind, making it a great way to de-stress and improve overall well-being.',
+  },
+  {
+    name: 'Stress Reduction',
+    description: 'Deep breathing serves as a mindfulness practice, allowing you to focus on the present moment and detach from stressful thoughts and worries. This can help reduce symptoms of anxiety and depression.',
+  },
+  {
+    name: 'Improved Respiratory Function',
+    description: 'Practicing deep breathing regularly can strengthen the diaphragm and improve lung capacity, leading to more efficient breathing patterns and better respiratory health.',
   },
   // Add more benefits as needed
 ];
@@ -23,10 +39,14 @@ const benefitsData = [
 const seedBenefit = async () => {
   await sequelize.sync({ force: true });
 
+
   await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
+
+
+
 
   await Benefit.bulkCreate(benefitsData);
 
