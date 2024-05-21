cb9e7d5803e02cba4f56268404cdacda804eccfc
rthomas86 <rebkthomas72@gmail.com>
2024-05-20 20:23:41 -0700

    trying to pull in correct version

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -73,31 +73,4 @@ router.get("/addBreath", withAuth, (req, res) => {
   res.render("addBreath", { logged_in: req.session.logged_in });
 });
 
-/*
-router.get("/", async (req, res) => {
-  try {
-
-    // Get all projects and JOIN with user data
-    const testData = await TestTable.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ["name"],
-        },
-      ],
-    });
-
-    // Serialize data so the template can read it
-    const tests = testData.map((test) => test.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    res.render("homepage", {
-      //logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-*/
-
 module.exports = router;
