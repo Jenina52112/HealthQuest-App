40aa4c71f8aaa2839957855c848fef1266071b22
AntCamper <152764140+AntCamper@users.noreply.github.com>
2024-05-20 17:52:30 -0700

    route page for home

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
 const helpers = require("./utils/helpers");
+const homepageRoute = require('./homepageRoute');
 // import sequelize connection
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
@@ -28,6 +29,8 @@ const sess = {
     db: sequelize,
   }),ee
 };
+
+app.use(homepageRoute);
 
 app.use(session(sess));
 
